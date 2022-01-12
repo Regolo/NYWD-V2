5d4fde51f7ba620069ab8959e44f65d54c97983e
Regolo <39636364+Regolo@users.noreply.github.com>
2022-01-12 18:01:26 +0100

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,383 @@
+/**
+ * main.js
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2016, Codrops
+ * http://www.codrops.com
+ */
+;(function(window) {
+
+	'use strict';
+
+	// Helper vars and functions.
+	function extend(a, b) {
+		for(var key in b) { 
+			if( b.hasOwnProperty( key ) ) {
+				a[key] = b[key];
+			}
+		}
+		return a;
+	}
+
+	function createDOMEl(type, className, content) {
+		var el = document.createElement(type);
+		el.className = className || '';
+		el.innerHTML = content || '';
+		return el;
+	}
+
+	/**
+	 * RevealFx obj.
+	 */
+	function RevealFx(el, options) {
+		this.el = el;
+		this.options = extend({}, this.options);
+		extend(this.options, options);
+		this._init();
+	}
+
+	/**
+	 * RevealFx options.
+	 */
+	RevealFx.prototype.options = {
+		// If true, then the content will be hidden until it´s "revealed".
+		isContentHidden: true,
+		// The animation/reveal settings. This can be set initially or passed when calling the reveal method.
+		revealSettings: {
+			// Animation direction: left right (lr) || right left (rl) || top bottom (tb) || bottom top (bt).
+			direction: 'rl',
+			// Revealer´s background color.
+			bgcolor: '#f0f0f0',
+			// Animation speed. This is the speed to "cover" and also "uncover" the element (seperately, not the total time).
+			duration: 500,
+			// Animation easing. This is the easing to "cover" and also "uncover" the element.
+			easing: 'easeInOutQuint',
+			// percentage-based value representing how much of the area should be left covered.
+			coverArea: 0,
+			// Callback for when the revealer is covering the element (halfway through of the whole animation).
+			onCover: function(contentEl, revealerEl) { return false; },
+			// Callback for when the animation starts (animation start).
+			onStart: function(contentEl, revealerEl) { return false; },
+			// Callback for when the revealer has completed uncovering (animation end).
+			onComplete: function(contentEl, revealerEl) { return false; }
+		}
+	};
+
+	/**
+	 * Init.
+	 */
+	RevealFx.prototype._init = function() {
+		this._layout();
+	};
+
+	/**
+	 * Build the necessary structure.
+	 */
+	RevealFx.prototype._layout = function() {
+		var position = getComputedStyle(this.el).position;
+		if( position !== 'fixed' && position !== 'absolute' && position !== 'relative' ) {
+			this.el.style.position = 'relative';
+		}
+		// Content element.
+		this.content = createDOMEl('div', 'block-revealer__content', this.el.innerHTML);
+		if( this.options.isContentHidden) {
+			this.content.style.opacity = 0;
+		}
+		// Revealer element (the one that animates)
+		this.revealer = createDOMEl('div', 'block-revealer__element');
+		this.el.classList.add('block-revealer');
+		this.el.innerHTML = '';
+		this.el.appendChild(this.content);
+		this.el.appendChild(this.revealer);
+	};
+
+	/**
+	 * Gets the revealer element´s transform and transform origin.
+	 */
+	RevealFx.prototype._getTransformSettings = function(direction) {
+		var val, origin, origin_2;
+
+		switch (direction) {
+			case 'lr' : 
+				val = 'scale3d(0,1,1)';
+				origin = '0 50%';
+				origin_2 = '100% 50%';
+				break;
+			case 'rl' : 
+				val = 'scale3d(0,1,1)';
+				origin = '100% 50%';
+				origin_2 = '0 50%';
+				break;
+			case 'tb' : 
+				val = 'scale3d(1,0,1)';
+				origin = '50% 0';
+				origin_2 = '50% 100%';
+				break;
+			case 'bt' : 
+				val = 'scale3d(1,0,1)';
+				origin = '50% 100%';
+				origin_2 = '50% 0';
+				break;
+			default : 
+				val = 'scale3d(0,1,1)';
+				origin = '0 50%';
+				origin_2 = '100% 50%';
+				break;
+		};
+
+		return {
+			// transform value.
+			val: val,
+			// initial and halfway/final transform origin.
+			origin: {initial: origin, halfway: origin_2},
+		};
+	};
+
+	/**
+	 * Reveal animation. If revealSettings is passed, then it will overwrite the options.revealSettings.
+	 */
+	RevealFx.prototype.reveal = function(revealSettings) {
+		// Do nothing if currently animating.
+		if( this.isAnimating ) {
+			return false;
+		}
+		this.isAnimating = true;
+		
+		// Set the revealer element´s transform and transform origin.
+		var defaults = { // In case revealSettings is incomplete, its properties deafault to:
+				duration: 500,
+				easing: 'easeInOutQuint',
+				delay: 0,
+				bgcolor: '#f0f0f0',
+				direction: 'lr',
+				coverArea: 0
+			},
+			revealSettings = revealSettings || this.options.revealSettings,
+			direction = revealSettings.direction || defaults.direction,
+			transformSettings = this._getTransformSettings(direction);
+
+		this.revealer.style.WebkitTransform = this.revealer.style.transform =  transformSettings.val;
+		this.revealer.style.WebkitTransformOrigin = this.revealer.style.transformOrigin =  transformSettings.origin.initial;
+		
+		// Set the Revealer´s background color.
+		this.revealer.style.backgroundColor = revealSettings.bgcolor || defaults.bgcolor;
+		
+		// Show it. By default the revealer element has opacity = 0 (CSS).
+		this.revealer.style.opacity = 1;
+
+		// Animate it.
+		var self = this,
+			// Second animation step.
+			animationSettings_2 = {
+				complete: function() {
+					self.isAnimating = false;
+					if( typeof revealSettings.onComplete === 'function' ) {
+						revealSettings.onComplete(self.content, self.revealer);
+					}
+				}
+			},
+			// First animation step.
+			animationSettings = {
+				delay: revealSettings.delay || defaults.delay,
+				complete: function() {
+					self.revealer.style.WebkitTransformOrigin = self.revealer.style.transformOrigin = transformSettings.origin.halfway;		
+					if( typeof revealSettings.onCover === 'function' ) {
+						revealSettings.onCover(self.content, self.revealer);
+					}
+					anime(animationSettings_2);		
+				}
+			};
+
+		animationSettings.targets = animationSettings_2.targets = this.revealer;
+		animationSettings.duration = animationSettings_2.duration = revealSettings.duration || defaults.duration;
+		animationSettings.easing = animationSettings_2.easing = revealSettings.easing || defaults.easing;
+
+		var coverArea = revealSettings.coverArea || defaults.coverArea;
+		if( direction === 'lr' || direction === 'rl' ) {
+			animationSettings.scaleX = [0,1];
+			animationSettings_2.scaleX = [1,coverArea/100];
+		}
+		else {
+			animationSettings.scaleY = [0,1];
+			animationSettings_2.scaleY = [1,coverArea/100];
+		}
+
+		if( typeof revealSettings.onStart === 'function' ) {
+			revealSettings.onStart(self.content, self.revealer);
+		}
+		anime(animationSettings);
+	};
+	
+	window.RevealFx = RevealFx;
+
+})(window);
+
+const labels = document.querySelectorAll(".accordion-item__label");
+const tabs = document.querySelectorAll(".accordion-tab");
+
+function toggleShow() {
+	const target = this;
+	const item = target.classList.contains("accordion-tab")
+		? target
+		: target.parentElement;
+	const group = item.dataset.actabGroup;
+	const id = item.dataset.actabId;
+
+	tabs.forEach(function(tab) {
+		if (tab.dataset.actabGroup === group) {
+			if (tab.dataset.actabId === id) {
+				tab.classList.add("accordion-active");
+			} else {
+				tab.classList.remove("accordion-active");
+			}
+		}
+	});
+
+	labels.forEach(function(label) {
+		const tabItem = label.parentElement;
+
+		if (tabItem.dataset.actabGroup === group) {
+			if (tabItem.dataset.actabId === id) {
+				tabItem.classList.add("accordion-active");
+			} else {
+				tabItem.classList.remove("accordion-active");
+			}
+		}
+	});
+}
+
+labels.forEach(function(label) {
+	label.addEventListener("click", toggleShow);
+});
+
+tabs.forEach(function(tab) {
+	tab.addEventListener("click", toggleShow);
+});
+
+
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+	// On-page links
+	if (
+	  location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+	  && 
+	  location.hostname == this.hostname
+	) {
+	  // Figure out element to scroll to
+	  var target = $(this.hash);
+	  target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+	  // Does a scroll target exist?
+	  if (target.length) {
+		// Only prevent default if animation is actually gonna happen
+		event.preventDefault();
+		$('html, body').animate({
+		  scrollTop: target.offset().top
+		}, 1000, function() {
+		  // Callback after animation
+		  // Must change focus!
+		  var $target = $(target);
+		  $target.focus();
+		  if ($target.is(":focus")) { // Checking if the target was focused
+			return false;
+		  } else {
+			$target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+			$target.focus(); // Set focus again
+		  };
+		});
+	  }
+	}
+  });
+
+$( document ).ready(function() {
+	$('.toggle-language').on('click', function(e) {
+		e.preventDefault();
+		$('.choose-language').toggleClass('open-language');
+	});
+	
+	scaleVideoContainer();
+	
+	initBannerVideoSize('.video-container .poster img');
+	initBannerVideoSize('.video-container .filter');
+	initBannerVideoSize('.video-container video');
+	
+	$(window).on('resize', function() {
+		scaleVideoContainer();
+		scaleBannerVideoSize('.video-container .poster img');
+		scaleBannerVideoSize('.video-container .filter');
+		scaleBannerVideoSize('.video-container video');
+	});
+	
+	
+
+});
+
+$('select.dropdown').each(function() {
+
+	var dropdown = $('<div />').addClass('dropdown selectDropdown');
+
+	$(this).wrap(dropdown);
+
+	var label = $('<span />').text($(this).attr('placeholder')).insertAfter($(this));
+	var list = $('<ul />');
+
+	$(this).find('option').each(function() {
+		list.append($('<li />').append($('<a />').text($(this).text())));
+	});
+
+	list.insertAfter($(this));
+
+	if($(this).find('option:selected').length) {
+		label.text($(this).find('option:selected').text());
+		list.find('li:contains(' + $(this).find('option:selected').text() + ')').addClass('active');
+		$(this).parent().addClass('filled');
+	}
+
+});
+
+$(document).on('click touch', '.selectDropdown ul li a', function(e) {
+	e.preventDefault();
+	var dropdown = $(this).parent().parent().parent();
+	var active = $(this).parent().hasClass('active');
+	var label = active ? dropdown.find('select').attr('placeholder') : $(this).text();
+
+	dropdown.find('option').prop('selected', false);
+	dropdown.find('ul li').removeClass('active');
+
+	dropdown.toggleClass('filled', !active);
+	dropdown.children('span').text(label);
+
+	if(!active) {
+		dropdown.find('option:contains(' + $(this).text() + ')').prop('selected', true);
+		$(this).parent().addClass('active');
+	}
+
+	dropdown.removeClass('open');
+});
+
+$('.dropdown > span').on('click touch', function(e) {
+	var self = $(this).parent();
+	self.toggleClass('open');
+});
+
+$(document).on('click touch', function(e) {
+	var dropdown = $('.dropdown');
+	if(dropdown !== e.target && !dropdown.has(e.target).length) {
+		dropdown.removeClass('open');
+	}
+});
+
+
+
+
+
+
+
+
